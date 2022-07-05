3aa2ff64c84e9dba669777bb2ec2ec02a5d14ca9
Your Name <development0261@gmail.com>
2022-07-05 15:57:35 +0530

    pg

--- django_prototype/settings.py
+++ django_prototype/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'django_prototype.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prototype',
+        'USER': 'admin',
+        'PASSWORD': 'Admin@123',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
 
 
 # Password validation
@@ -121,9 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 CELERY_BROKER_URL = 'redis://localhost:6379'
