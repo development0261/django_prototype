3a9b34d59e34ae36e1592e92f96beff42819f0ac
Your Name <development0261@gmail.com>
2022-07-14 16:52:21 +0530

    redis

--- django_prototype/celery.py
+++ django_prototype/celery.py
@@ -3,9 +3,15 @@ import os
 from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
+import ssl
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_prototype.settings')
-app = Celery('django_prototype',include=['pod.task'])
+app = Celery('django_prototype',broker_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     },
+     redis_backend_use_ssl = {
+        'ssl_cert_reqs': ssl.CERT_NONE
+     },include=['pod.task'])
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
